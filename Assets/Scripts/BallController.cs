c1d066f186cf5f8165fb3c143c7d4315362b4849
steven.liatti <steven.liatti@etu.hesge.ch>
2018-04-14 17:05:29 +0200

    Add table and balls with textures and materials
    On branch master
    
    Changes to be committed:
    	new file:   Materials/Ball1.mat
    	new file:   Materials/Ball1.mat.meta
    	new file:   Materials/Ball10.mat
    	new file:   Materials/Ball10.mat.meta
    	new file:   Materials/Ball11.mat
    	new file:   Materials/Ball11.mat.meta
    	new file:   Materials/Ball12.mat
    	new file:   Materials/Ball12.mat.meta
    	new file:   Materials/Ball13.mat
    	new file:   Materials/Ball13.mat.meta
    	new file:   Materials/Ball14.mat
    	new file:   Materials/Ball14.mat.meta
    	new file:   Materials/Ball15.mat
    	new file:   Materials/Ball15.mat.meta
    	new file:   Materials/Ball2.mat
    	new file:   Materials/Ball2.mat.meta
    	new file:   Materials/Ball3.mat
    	new file:   Materials/Ball3.mat.meta
    	new file:   Materials/Ball4.mat
    	new file:   Materials/Ball4.mat.meta
    	new file:   Materials/Ball5.mat
    	new file:   Materials/Ball5.mat.meta
    	new file:   Materials/Ball6.mat
    	new file:   Materials/Ball6.mat.meta
    	new file:   Materials/Ball7.mat
    	new file:   Materials/Ball7.mat.meta
    	new file:   Materials/Ball8.mat
    	new file:   Materials/Ball8.mat.meta
    	new file:   Materials/Ball9.mat
    	new file:   Materials/Ball9.mat.meta
    	new file:   Materials/BallCue.mat
    	new file:   Materials/BallCue.mat.meta
    	new file:   Materials/TableMaterial.mat
    	new file:   Materials/TableMaterial.mat.meta
    	deleted:    Objects/CueAlone.obj
    	deleted:    Objects/CueAlone.obj.meta
    	new file:   Objects/Pool Table.3DS
    	new file:   Objects/Pool Table.3DS.meta
    	deleted:    Objects/PoolTableAlone.obj
    	deleted:    Objects/PoolTableAlone.obj.meta
    	modified:   Prefabs/Ball.prefab
    	modified:   Scenes/billard.unity
    	modified:   Scripts/BallController.cs
    	new file:   Textures.meta
    	new file:   Textures/Ball 15.jpg
    	new file:   Textures/Ball 15.jpg.meta
    	new file:   Textures/Ball1.jpg
    	new file:   Textures/Ball1.jpg.meta
    	new file:   Textures/Ball10.jpg
    	new file:   Textures/Ball10.jpg.meta
    	new file:   Textures/Ball11.jpg
    	new file:   Textures/Ball11.jpg.meta
    	new file:   Textures/Ball12.jpg
    	new file:   Textures/Ball12.jpg.meta
    	new file:   Textures/Ball13.jpg
    	new file:   Textures/Ball13.jpg.meta
    	new file:   Textures/Ball14.jpg
    	new file:   Textures/Ball14.jpg.meta
    	new file:   Textures/Ball2.jpg
    	new file:   Textures/Ball2.jpg.meta
    	new file:   Textures/Ball3.jpg
    	new file:   Textures/Ball3.jpg.meta
    	new file:   Textures/Ball4.jpg
    	new file:   Textures/Ball4.jpg.meta
    	new file:   Textures/Ball5.jpg
    	new file:   Textures/Ball5.jpg.meta
    	new file:   Textures/Ball6.jpg
    	new file:   Textures/Ball6.jpg.meta
    	new file:   Textures/Ball7.jpg
    	new file:   Textures/Ball7.jpg.meta
    	new file:   Textures/Ball8.jpg
    	new file:   Textures/Ball8.jpg.meta
    	new file:   Textures/Ball9.jpg
    	new file:   Textures/Ball9.jpg.meta
    	new file:   Textures/BallCue.jpg
    	new file:   Textures/BallCue.jpg.meta

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -1,16 +1,24 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class BallController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public float speed;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
 }
