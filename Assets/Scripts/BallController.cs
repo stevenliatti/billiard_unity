fb3c6489d2314f4a48f3d9ec8ce387a897241e44
steven.liatti <steven.liatti@etu.hesge.ch>
2018-04-15 16:27:38 +0200

    Control white ball done
    On branch master
    
    Changes to be committed:
    	modified:   Assets/Scenes/billard.unity
    	modified:   Assets/Scripts/BallController.cs
    	new file:   Assets/Scripts/ScoreAndCollision.cs
    	new file:   Assets/Scripts/ScoreAndCollision.cs.meta
    	renamed:    Assets/Textures/Ball 15.jpg -> Assets/Textures/Ball15.jpg
    	renamed:    Assets/Textures/Ball 15.jpg.meta -> Assets/Textures/Ball15.jpg.meta
    	modified:   ProjectSettings/TagManager.asset

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -3,22 +3,54 @@ using UnityEngine;
 
 public class BallController : MonoBehaviour {
 
-    public float speed;
+    public static int score1 = 0;
+    public static int score2 = 0;
+	
+    public static Rigidbody rb;
 
-    private Rigidbody rb;
+    public float moveVertical;
+    public float speed;
+    public Vector3 playerPosition;
+    public Vector3 endLine;
+    public Vector3 lineSize;
+    public LineRenderer line;
+
 
     void Start()
     {
+        speed = 10;
+        moveVertical = 0;
         rb = GetComponent<Rigidbody>();
+        lineSize = new Vector3(10.0f, 0.0f, 0.0f);
+        playerPosition = rb.transform.position;
+        endLine = playerPosition + lineSize;
+        line = gameObject.AddComponent<LineRenderer>();
     }
 
     void FixedUpdate()
     {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
+        playerPosition = rb.transform.position;
 
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            Quaternion rotate = Quaternion.Euler(0, -2, 0);
+            lineSize = rotate * lineSize;
+        }
 
-        rb.AddForce(movement * speed);
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            Quaternion rotate = Quaternion.Euler(0, 2, 0);
+            lineSize = rotate * lineSize;
+        }
+
+        endLine = playerPosition + lineSize;
+		moveVertical += Input.GetAxis("Vertical");
+        endLine += lineSize.normalized * moveVertical;
+        line.SetPosition(0, playerPosition);
+        line.SetPosition(1, endLine);
+
+        if (Input.GetKey(KeyCode.Space)) {
+            rb.AddForce((endLine - playerPosition) * speed);
+        }
     }
 }
---
+++ Assets/Scripts/ScoreAndCollision.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreAndCollision : MonoBehaviour {
+
+    void Update() {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player")) {
+            other.transform.position = new Vector3(-23f, 25.95059f, 0f);
+            BallController.rb.velocity = Vector3.zero;
+            Debug.Log("Player in hole");
+        }
+        if (other.gameObject.CompareTag("full")) {
+            BallController.score1++;
+            Debug.Log("full : " + BallController.score1.ToString());
+            Destroy(other.gameObject);
+        }
+        if (other.gameObject.CompareTag("half_full")) {
+            BallController.score2++;
+            Debug.Log("half_full : " + BallController.score2.ToString());
+            Destroy(other.gameObject);
+        }
+
+    }
+}
